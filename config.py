b74310921ceb2389e5f6f813e4e4360c3b693dc2
vinh-nguyenxuan <huskypeo2008@gmail.com>
2024-10-16 21:31:50 +0700

    init project

---
+++ config.py
@@ -0,0 +1,14 @@
+BATCH_SIZE = 256
+SAVE_FREQ = 10
+TEST_FREQ = 5
+TOTAL_EPOCH = 70
+
+RESUME = ''
+SAVE_DIR = './model'
+MODEL_PRE = 'CASIA_ShuffleFaceNet_'
+
+
+CASIA_DATA_DIR = 'C:/Users/TUF/Downloads/faces_emore/' #'/'CASIA-WebFace
+LFW_DATA_DIR = 'C:/Users/TUF/Downloads/faces_emore/faces_emore_112x112_folders/'
+
+GPU = 0,1,2
